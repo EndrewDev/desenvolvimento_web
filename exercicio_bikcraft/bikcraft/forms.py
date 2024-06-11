5085183fe764ce43a26d47936d9ad8a0ca65ef54
Endrew Sousa <endrew.developer@gmail.com>
2024-06-11 20:16:11 -0300

    atualizada

--- exercicio_bikcraft/bikcraft/forms.py
+++ exercicio_bikcraft/bikcraft/forms.py
@@ -16,3 +16,22 @@ class bikeform(forms.Form):
             )
         bike.save()
         return bike
+
+class BikeModelForm(forms.ModelForm):
+    class meta:
+        model = Bike
+        fields = '__all__'
+
+    def clean_preco(self):
+        preco_cadastra = self.cleaned_data['preco']
+        if preco_cadastra < 200:
+            self.add_error('preco', 'O preço não pode ser menor do que R$200')
+        return preco_cadastra
+    
+    def clean_foto(self):
+        tamanho_maximo = 2
+        imagem = self.cleaned_data('foto')
+        tamanho_imagem = tamanho_maximo * 1024 * 1024
+        if imagem.size > tamanho_imagem:
+            return forms.ValidationError('A imagem tem um tamanho maior que o suportado')
+        return imagem
--- exercicio_bikcraft/bikcraft/models.py
+++ exercicio_bikcraft/bikcraft/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 class Bike(models.Model):
-    modelo = models.CharField(max_length=255)
-    preco = models.FloatField()
-    descricao = models.TextField()
-    foto = models.ImageField(upload_to='bikcraft/', blank=True, null=True)
+    modelo = models.CharField(max_length=255, verbose_name='Modelo')
+    preco = models.FloatField(verbose_name='Preço')
+    descricao = models.TextField(verbose_name='Descrição')
+    foto = models.ImageField(upload_to='bikcraft/', blank=True, null=True, verbose_name='Foto')
 
     def __str__(self):
         return self.modelo
     
 class Contados(models.Model):
-    nome = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    mensagem = models.TextField()
+    nome = models.CharField(max_length=255, verbose_name='Nome')
+    email = models.CharField(max_length=255, verbose_name='E-mail')
+    mensagem = models.TextField(verbose_name='Mensagem')
 
     def __str__(self):
         return self.nome
