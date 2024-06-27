695217eb335c999990b6be03ea3dcf8b83f41750
Endrew Sousa <endrew.developer@gmail.com>
2024-06-27 19:25:17 -0300

    atualizada

--- exercicio_bikcraft/contas/views.py
+++ exercicio_bikcraft/contas/views.py
@@ -3,31 +3,35 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+# cadastra
 def cadastrar_usuario(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserCreationForm(data=request.POST)
         if form.is_valid():
             form.save()
             return redirect('pagina-inicial')
-        else:
-            return redirect('pagina-erro')
-    form = UserCreationForm()
+    else:
+        form = UserCreationForm()
     return render(request, 'cadastra_usuario.html', {'form': form})
 
+# acessa
 def acessar(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
 
-        if user:
-            login(request, user)
-            return redirect('pagina-inicial')
-        else:
-            return redirect('erro-acessa')
-    form = AuthenticationForm()
+            if user is not None:
+                login(request, user)
+                return redirect('pagina-inicial')
+    else:
+        form = AuthenticationForm()
     return render(request, 'acessa.html', {'form': form})
 
+# sair
 def sair(request):
     logout(request)
     return redirect('pagina-inicial')
