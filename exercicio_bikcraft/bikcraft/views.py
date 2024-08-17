fa2ead8c90909f2043eb85b46e68768d61d7cf71
Endrew Sousa <endrew.developer@gmail.com>
2024-08-17 16:29:33 -0300

    atualizada para 17-8-24

--- exercicio_bikcraft/bikcraft/views.py
+++ exercicio_bikcraft/bikcraft/views.py
@@ -52,8 +52,8 @@ def atualizado_produtos(request, pk):
     return render(request, 'cadastra_produtos_atualizar.html', {'form': form, 'bike': bike})
 
 # deleta produto:
-def deleta_produto(request, pk):
-    deleta_produtos = get_object_or_404(Bike, pk=pk)
+def deleta_produto(request, id):
+    deleta_produtos = get_object_or_404(Bike, id=id)
     deleta_produtos.delete()
     return redirect('pagina-produtos')
 
